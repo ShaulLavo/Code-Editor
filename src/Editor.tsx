6fdf052bc0239ca4792d8af6364d5eb77f07fe57
Shauls <shaul919191@gmail.com>
2024-05-27 00:08:43 +0300

    mac fix

--- src/Editor.tsx
+++ src/Editor.tsx
@@ -1,47 +1,87 @@
 import { createCodeMirror, createEditorControlledValue } from 'solid-codemirror'
-import { Accessor, Setter, onMount } from 'solid-js'
+import {
+	Accessor,
+	Setter,
+	createEffect,
+	createResource,
+	onMount
+} from 'solid-js'
 import { ThemeKey, setTheme } from './themeStore'
 import { createDefaultExtensions } from './utils/extensions'
-import { createShortcut } from '@solid-primitives/keyboard'
+import {
+	createShortcut,
+	useKeyDownEvent,
+	useKeyDownList
+} from '@solid-primitives/keyboard'
 import { formatCode } from './format'
 
 export interface EditorProps {
-  code: Accessor<string>
-  setCode: Setter<string>
-  defaultTheme?: ThemeKey
-  showLineNumber?: Accessor<boolean>
+	code: Accessor<string>
+	setCode: Setter<string>
+	defaultTheme?: ThemeKey
+	showLineNumber?: Accessor<boolean>
+	formatOnMount?: Accessor<boolean>
 }
 
 export const Editor = ({
-  code,
-  setCode,
-  defaultTheme,
-  showLineNumber
+	code,
+	setCode,
+	defaultTheme,
+	showLineNumber,
+	formatOnMount = () => true
 }: EditorProps) => {
-  const {
-    ref: EditorRef,
-    createExtension,
-    editorView
-  } = createCodeMirror({
-    onValueChange: setCode,
-    value: code()
-  })
-  //this function does a heavy string compare
-  createEditorControlledValue(editorView, code)
-  createShortcut(
-    ['Alt', 'Shift', 'F'],
-    async () => {
-      console.log('alt shift f', editorView())
-      const formatted = await formatCode(code())
-      setCode(formatted)
-    },
-    { preventDefault: true, requireReset: true }
-  )
-  createDefaultExtensions(createExtension, showLineNumber)
-  defaultTheme && setTheme(defaultTheme)
-  onMount(() => {
-    console.log(editorView())
-  })
+	const {
+		ref: EditorRef,
+		createExtension,
+		editorView
+	} = createCodeMirror({
+		onValueChange: setCode,
+		value: code()
+	})
 
-  return <div ref={EditorRef} />
+	//this function does a heavy string compare
+	createEditorControlledValue(editorView, code)
+	createShortcut(
+		['Alt', 'Shift', 'F'],
+		async () => {
+			console.log('alt shift f', editorView())
+			const formatted = await formatCode(code())
+			setCode(formatted)
+		},
+		{ preventDefault: true, requireReset: true }
+	)
+	createShortcut(
+		['Alt', 'Shift', 'F'],
+		async () => {
+			console.log('alt shift f', editorView())
+			const formatted = await formatCode(code())
+			setCode(formatted)
+		},
+		{ preventDefault: true, requireReset: true }
+	)
+	createShortcut(
+		['Alt', 'Shift', 'Ï'],
+		async () => {
+			console.log('alt shift f', editorView())
+			const formatted = await formatCode(code())
+			setCode(formatted)
+		},
+		{ preventDefault: true, requireReset: true }
+	)
+
+	const key = useKeyDownList()
+	createEffect(() => {
+		console.log(key())
+	})
+
+	createDefaultExtensions(createExtension, showLineNumber)
+	defaultTheme && setTheme(defaultTheme)
+	onMount(async () => {
+		if (formatOnMount()) {
+			//maybe add a blocking before mount
+			setCode(await formatCode(code()))
+		}
+	})
+
+	return <div ref={EditorRef} />
 }
